220ebd2181e1e66b705b935023751590c634a987
TomasEscudero <escudero17756@iesmarenostrum.com>
2017-05-07 18:16:07 +0200

    Aplicacción 1 CambioDivisas Refactorizada a Clase Estática

---
+++ NavajaValirya/NavajaValirya/Aplicacion 1/CambioDivisaLogica.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Namespace de la Clase CambioDivisaLogica del proyecto NavajaValirya.
+/// </summary>
+namespace NavajaValirya.CambioDivisa
+{
+    /// <summary>
+    /// Clase que ejecuta los métodos necesarios para la aplicación 1 que corresponde a CambioDivisa.
+    /// <para>Clase que realiza el cambio entre las divisas Euros y Pesetas y viceversa.</para>
+    /// </summary>
+    public class CambioDivisaLogica
+    {
+        /// <summary>
+        /// Es la constante para realizar el cambio entre las divisas.
+        /// </summary>
+        /// <value>1 Euro = 166.386 pesetas</value>
+        const double kVALOR = 166.386;
+
+        /// <summary>
+        /// Función cambiarEuros.
+        /// <para>Realiza el cambio de Euros a Pesetas.</para>
+        /// </summary>
+        /// <param name="cantidadEuros">El valor del parámetro <paramref name="cantidadEuros"/> es la cantidad de euros que se quiere cambiar a pesetas.</param>
+        /// <remarks>Se pueden introducir cantidades con céntimos de euro.</remarks>
+        /// <returns name="resultadoCambio">Devuelve la <paramref name="cantidadEuros"/> cambiada en pesetas.</returns>
+        /// <value>El dinero cambiado a pesetas.</value>
+        public static double cambiarEuros(double cantidadEuros)
+        {
+            double resultadoCambio;
+
+            resultadoCambio = cantidadEuros / kVALOR;
+
+            return resultadoCambio;
+        }
+
+        /// <summary>
+        /// Función cambiarPesetas.
+        /// <para>Realiza el cambio de Pesetas a Euros.</para>
+        /// </summary>
+        /// <param name="cantidadPesetas">El valor del parámetro <paramref name="cantidadPesetas"/> es la cantidad de pesetas que se quiere cambiar a euros.</param>
+        /// <remarks>No se pueden introducir cantidades con decimales ya que las pesetas no tenían centimos.</remarks>
+        /// <returns name="resultadoCambio">Devuelve la <paramref name="cantidadPesetas"/> cambiada en euros.</returns>
+        /// <value>El dinero cambiado a euros.</value>
+        public static double cambiarPesetas(double cantidadPesetas)
+        {
+            double resultadoCambio;
+
+            resultadoCambio = cantidadPesetas * kVALOR;
+
+            return resultadoCambio;
+        }
+    }
+}
--- NavajaValirya/NavajaValirya/Aplicacion 1/formCambioDivisa.cs
+++ NavajaValirya/NavajaValirya/Aplicacion 1/formCambioDivisa.cs
@@ -26,96 +26,56 @@ namespace NavajaValirya.CambioDivisa
         {
             InitializeComponent();
         }
+        
+    /// <summary>
+    /// Lee la cantidad de la caja de texto TCantidad y ejecuta la función estática cambiarEuros de la clase CambioDivisaLogica.
+    /// </summary>
+    /// <param name="sender">Lanza el evento el botón BCambioEuros</param>
+    /// <param name="e">Sin uso</param>        
+    private void BCambioEnEuros_Click(object sender, EventArgs e)
+    {
+        double cantidadPesetas, resultadoEuros;
 
-        /// <summary>
-        /// Es la constante para realizar el cambio entre las divisas.
-        /// </summary>
-        /// <value>1 Euro = 166.386 pesetas</value>
-        const double kVALOR = 166.386;
+        cantidadPesetas = 0;
+        resultadoEuros = 0;
 
-        /// <summary>
-        /// Función cambiarEuros.
-        /// <para>Realiza el cambio de Euros a Pesetas.</para>
-        /// </summary>
-        /// <param name="cantidadEuros">El valor del parámetro <paramref name="cantidadEuros"/> es la cantidad de euros que se quiere cambiar a pesetas.</param>
-        /// <remarks>Se pueden introducir cantidades con céntimos de euro.</remarks>
-        /// <returns name="resultadoCambio">Devuelve la <paramref name="cantidadEuros"/> cambiada en pesetas.</returns>
-        /// <value>El dinero cambiado a pesetas.</value>
-        private double cambiarEuros(double cantidadEuros)
+        if (double.TryParse(TCantidad.Text, out cantidadPesetas))
         {
-            double resultadoCambio;
-
-            resultadoCambio = cantidadEuros / kVALOR;
-
-            return resultadoCambio;
+            resultadoEuros = CambioDivisaLogica.cambiarEuros(cantidadPesetas);
+            MessageBox.Show("El cambio son: " + resultadoEuros + "€.");
         }
 
-        /// <summary>
-        /// Función cambiarPesetas.
-        /// <para>Realiza el cambio de Pesetas a Euros.</para>
-        /// </summary>
-        /// <param name="cantidadPesetas">El valor del parámetro <paramref name="cantidadPesetas"/> es la cantidad de pesetas que se quiere cambiar a euros.</param>
-        /// <remarks>No se pueden introducir cantidades con decimales ya que las pesetas no tenían centimos.</remarks>
-        /// <returns name="resultadoCambio">Devuelve la <paramref name="cantidadPesetas"/> cambiada en euros.</returns>
-        /// <value>El dinero cambiado a euros.</value>
-        private double cambiarPesetas(double cantidadPesetas)
+        else
         {
-            double resultadoCambio;
-
-            resultadoCambio = cantidadPesetas * kVALOR;
-
-            return resultadoCambio;
+            MessageBox.Show("No ha introducido valor correcto, por favor, introduzca un número.");
         }
 
-        /// <summary>
-        /// Lee la cantidad de la caja de texto TCantidad y ejecuta la función cambiarEuros.
-        /// </summary>
-        /// <param name="sender">Lanza el evento el botón BCambioEuros</param>
-        /// <param name="e">Sin uso</param>        
-        private void BCambioEnEuros_Click(object sender, EventArgs e)
+    }
+
+    /// <summary>
+    /// Lee la cantidad de la caja de texto TCantidad y ejecuta la función estática cambiarPesetas de la clase CambioDivisaLogica.
+    /// </summary>
+    /// <param name="sender">Lanza el evento del botón BCambioEnPesetas</param>
+    /// <param name="e">Sin uso</param>
+    private void BCambioEnPesetas_Click(object sender, EventArgs e)
+    {
+        double cantidadEuros, resultadoPesetas;
+
+        cantidadEuros = 0;
+        resultadoPesetas = 0;
+
+        if (double.TryParse(TCantidad.Text, out cantidadEuros))
         {
-            double cantidadPesetas, resultadoEuros;
-
-            cantidadPesetas = 0;
-            resultadoEuros = 0;
-
-            if (double.TryParse(TCantidad.Text, out cantidadPesetas))
-            {
-                resultadoEuros = cambiarEuros(cantidadPesetas);
-                MessageBox.Show("El cambio son: " + resultadoEuros + "€.");
-            }
-
-            else
-            {
-                MessageBox.Show("No ha introducido valor correcto, por favor, introduzca un número.");
-            }            
-                        
+            resultadoPesetas = CambioDivisaLogica.cambiarPesetas(cantidadEuros);
+            MessageBox.Show("El cambio son: " + resultadoPesetas + "ptas.");
         }
 
-        /// <summary>
-        /// Lee la cantidad de la caja de texto TCantidad y ejecuta la función cambiarPesetas.
-        /// </summary>
-        /// <param name="sender">Lanza el evento del botón BCambioEnPesetas</param>
-        /// <param name="e">Sin uso</param>
-        private void BCambioEnPesetas_Click(object sender, EventArgs e)
+        else
         {
-            double cantidadEuros, resultadoPesetas;
-
-            cantidadEuros = 0;
-            resultadoPesetas = 0;
-
-            if (double.TryParse(TCantidad.Text, out cantidadEuros))
-            {
-                resultadoPesetas = cambiarPesetas(cantidadEuros);
-                MessageBox.Show("El cambio son: " + resultadoPesetas + "ptas.");
-            }
-
-            else
-            {
-                MessageBox.Show("No ha introducido un valor correcto, por favor, introduzca un número.");
-            }                       
-
-            
+            MessageBox.Show("No ha introducido un valor correcto, por favor, introduzca un número.");
         }
+
+
+    }
     }
 }
